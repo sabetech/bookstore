75196c91ad522f601a443424f18ca7b286c95fd9
Albert <albert.mensahansah@gmail.com>
2022-05-19 15:07:40 +0000

    styled book store according to zepline prototype

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBookApi } from '../redux/books/books';
 import styles from '../modules/BookItem.module.css';
+import './bookitem.css';
 
 const BookItem = ({ id, title, author }) => {
   const dispatch = useDispatch();
@@ -13,6 +14,9 @@ const BookItem = ({ id, title, author }) => {
   return (
     <li className={styles.item}>
       <div className={styles.bookInfo}>
+        <div className={styles.genre}>
+          Thriller
+        </div>
         <div className={styles.bookTitle}>
           { title }
         </div>
@@ -21,19 +25,32 @@ const BookItem = ({ id, title, author }) => {
         </div>
         <ul className={styles.actionLinks}>
           <li>
-            <button type="button">Comments</button>
-            {' '}
-            |
+            <button type="button" className={styles.comments}>Comments</button>
           </li>
           <li className={styles.actionLinkItem}>
-            <button type="button" onClick={(e) => handleDelete(e, id)}>Remove</button>
-            {' '}
-            |
+            <button type="button" className={styles.buttonLink} onClick={(e) => handleDelete(e, id)}>Remove</button>
           </li>
           <li className={styles.actionLinkItem}>
-            <button type="button">Edit</button>
+            <button type="button" className={styles.buttonLink}>Edit</button>
           </li>
         </ul>
+      </div>
+      <div className={styles.progressInfo}>
+        <div className="circle-bar" />
+        <div className={styles.progressSubInfo}>
+          <p className={styles.progressPercent}>
+            64%
+          </p>
+          <p>Completed</p>
+        </div>
+      </div>
+      <div className={styles.vl} />
+      <div className={styles.chapterInfo}>
+        <div className={styles.chapterSubInfo}>
+          <p className={styles.currentChapter}>CURRENT CHAPTER</p>
+          <p>Chapter 17</p>
+          <button type="button" className={styles.buttonProgress}>UPDATE PROGRESS</button>
+        </div>
       </div>
     </li>
   );
